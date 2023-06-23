486d21af2435c40581b3edc7110dbb89710fa6f1
Facundo Perez Schneider <facup200718@gmail.com>
2023-06-23 12:01:51 -0300

    Desafío clase 8

--- src/com/anncode/makereport/Main.java
+++ src/com/anncode/makereport/Main.java
@@ -1,7 +1,26 @@
 package com.anncode.makereport;
 
+/**
+ *
+ * <h1>MakeReport</h1>
+ * Clase principal del programa MakeReport.
+ * MakeReport es una biblioteca que te permite escribir archivos.
+ * <p>
+ * Esta clase muestra un ejemplo de uso de la biblioteca MakeReport para escribir un archivo.
+ *
+ * @author Facu
+ * @version 1.1
+ * @since 2023
+ *
+ */
 public class Main {
 
+	/**
+	 * Método principal que demuestra el uso de la biblioteca MakeReport para escribir un archivo.
+	 * Crea un objeto Report, configura sus atributos y se escribe el archivo.
+	 *
+	 * @param args Los argumentos de línea de comandos. No se utilizan en este ejemplo.
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
--- src/com/anncode/makereport/Report.java
+++ src/com/anncode/makereport/Report.java
@@ -7,32 +7,88 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
+/**
+ *
+ * <h1>Report</h1>
+ * Report es una clase que representa los archivos a escribir.
+ * <p>
+ * Report contiene el método {@code makeReport()} que se encargará de escribir un archivo.
+ * <br/>
+ * Las propiedades de un objeto producto de una instancia de la clase Report son: {@code nameFile}, {@code title},
+ * {@code content}, y {@code extension}. Cada propiedad tiene sus respectivos métodos getter y setter.
+ *
+ * @author Facu
+ * @version 1.1
+ * @since 2023
+ *
+ */
 public class Report {
 	
 	private String nameFile;
 	private String title;
 	private String content;
 	private String extension;
-	
+
+	/**
+	 * Devolver el nombre del archivo.
+	 * @return El nombre del archivo.
+	 */
 	public String getNameFile() {
 		return nameFile;
 	}
+
+	/**
+	 * Setear el nombre del archivo.
+	 * @param nameFile
+	 */
 	public void setNameFile(String nameFile) {
 		this.nameFile = nameFile;
 	}
+
+	/**
+	 * Devolver el título del archivo
+	 * @return El título del archivo.
+	 */
 	public String getTitle() {
 		return title;
 	}
+
+	/**
+	 * Setear el título del archivo.
+	 * @param title
+	 */
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
+	/**
+	 * Devolver el contenido del archivo
+	 * @return El contenido del archivo.
+	 */
 	public String getContent() {
 		return content;
 	}
+
+	/**
+	 * Setear el contenido del archivo.
+	 * @param content
+	 */
 	public void setContent(String content) {
 		this.content = content;
 	}
-	
+
+	/**
+	 * Crear un archivo a partir de los siguientes atributos:
+	 * <ul>
+	 *     <li>{@code nameFile}</li>
+	 *     <li>{@code title}</li>
+	 *     <li>{@code content}</li>
+	 *     <li>{@code extension}</li>
+	 * </ul>
+	 *
+	 * @return El método no devuelve un valor ya que su output es el archivo ya escrito.
+	 * @throws IOException Si hay un problema en la escritura del archivo.
+	 */
 	public void makeReport() {
 		if ( (getNameFile() != null) && (getTitle() != null) && (getContent() != null) ) {
 			//Crear el archivo
@@ -54,12 +110,20 @@ public class Report {
 			System.out.println("Ingresa los datos del archivo");
 		}
 	}
-	
-	
-	
+
+
+	/**
+	 * Devolver la extensión del archivo
+	 * @return La extensión del archivo.
+	 */
 	public String getExtension() {
 		return extension;
 	}
+
+	/**
+	 * Setear la extensión del archivo.
+	 * @param extension
+	 */
 	public void setExtension(String extension) {
 		this.extension = extension;
 	}
