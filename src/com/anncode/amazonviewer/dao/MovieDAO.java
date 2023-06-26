9790bda8e566b64ce7da4f982c1de6bda312e5cd
Facundo Perez Schneider <facup200718@gmail.com>
2023-06-26 14:11:06 -0300

    Clase 29

--- src/com/anncode/amazonviewer/dao/MovieDAO.java
+++ src/com/anncode/amazonviewer/dao/MovieDAO.java
@@ -9,6 +9,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import static com.anncode.amazonviewer.db.Database.*;
 
 /**
  * Es el equivalente a las clases Repository de la actualidad
@@ -22,16 +23,17 @@ public interface MovieDAO extends IDBConnection {
     default ArrayList<Movie> read() {
         ArrayList<Movie> movies = new ArrayList<>();
         try (Connection connection = connectToDB()) {
-            String query = "SELECT * FROM " + Database.MOVIE_TABLE;
+            String query = "SELECT * FROM " + MOVIE_TABLE;
             PreparedStatement preparedStatement = connection.prepareStatement(query);
             ResultSet rs = preparedStatement.executeQuery();
             while (rs.next()) {
-                var movie = new Movie(rs.getString(Database.MOVIE_TITLE),
-                        rs.getString(Database.MOVIE_GENRE),
-                        rs.getString(Database.MOVIE_DIRECTOR),
-                        Integer.valueOf(rs.getString(Database.MOVIE_DURATION)),
-                        Short.valueOf(rs.getString(Database.MOVIE_YEAR)));
-                movie.setId(Integer.valueOf(rs.getString(Database.MOVIE_ID)));
+                var movie = new Movie(rs.getString(MOVIE_TITLE),
+                        rs.getString(MOVIE_GENRE),
+                        rs.getString(MOVIE_DIRECTOR),
+                        Integer.valueOf(rs.getString(MOVIE_DURATION)),
+                        Short.valueOf(rs.getString(MOVIE_YEAR)));
+                movie.setId(Integer.valueOf(rs.getString(MOVIE_ID)));
+                movie.setViewed(getMovieViewedStatus(preparedStatement, connection, movie.getId()));
                 movies.add(movie);
             }
         } catch (SQLException e) {
@@ -40,7 +42,21 @@ public interface MovieDAO extends IDBConnection {
         return movies;
     }
 
-    private Boolean getMovieViewedStatus() {
-        return Boolean.FALSE;
+    private Boolean getMovieViewedStatus(PreparedStatement preparedStatement, Connection connection, int movieId) {
+        Boolean viewedStatus = Boolean.FALSE;
+        String query = "SELECT * FROM " + VIEWED_TABLE + " WHERE " + VIEWED_MATERIAL_ID + " = ? "
+                + "AND " + VIEWED_ELEMENT_ID + " = ? AND " + VIEWED_USER_ID + " = ?";
+        ResultSet rs = null;
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, MATERIAL_IDS[0]);
+            preparedStatement.setInt(2, movieId);
+            preparedStatement.setInt(3, USER_ID);
+            rs = preparedStatement.executeQuery();
+            viewedStatus = rs.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return viewedStatus;
     }
 }
